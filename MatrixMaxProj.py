809cd236d59e1e01154acd11ac9c21ee77be65fe
peiyaow <monicawang76@gmail.com>
2018-09-18 23:42:10 +0000

    one subject

---
+++ MatrixMaxProj.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 10:56:03 2018
+
+@author: peiyao
+"""
+
+import rpy2 
+import rpy2.robjects as robjects
+# import rpy2.robjects.packages as rpackages
+robjects.r('''
+        GetSigX = function(A, eigen.threshold = 0.01){
+        	# this function is to make sure the matrix have lowest positive eigen value
+        	# This will keep the same eigen vector but only threshold the eigen value to
+        	# a presepecified eigen value
+        	eig.A = eigen(A)$value
+        	id = (eig.A < eigen.threshold)
+        	eig.A[id] = eigen.threshold
+        	result = eigen(A)$vectors %*% diag(eig.A) %*% t(eigen(A)$vectors)
+        	return(result)	
+        }
+        
+        MatrixMaxProj = function(M, r = 0.5, tol_value = 0.01, N = 100) {
+        	#This is the projection to make the matrix positive definite
+        	t = 0  
+        	p = dim(M)[1]
+        	Rt = GetSigX(M, 0.01)
+        	Zt = matrix(rep(1/p^2, p^2), p, p)
+        	while(t < N){ 
+        		R0t = Rt - GetSigX(Rt - Zt, eigen.threshold = 0) # ex
+        		Z0t = Zt - PB1.projection(Rt + Zt - M)     # ez
+        		diff = max(max(abs(R0t)), max(abs(Z0t)))
+        		cat(sep = "", "[",t,"]","dif=", round(diff, 3), "\n")
+        		if(diff < tol_value){
+        			break
+        		}
+        		Rt = Rt - r * (R0t - Z0t)/2
+        		Zt = Zt - r * (R0t + Z0t)/2
+        		t = t + 1
+        	}
+        	A = as.matrix(Rt)
+        	return(A)
+        }
+        
+        PB1.projection = function(A){
+        	# This is to find matrix A.result where |A.result|_1 <=1
+        	# min ||  A - A.result||_F
+        	p = dim(A)[1]
+        	a = as.vector(A)
+        	T.mat = T.matrix(A * sign(A))
+        	a1 = T.mat %*% (sign(a) * a)  
+        	a.del = Delta.vec(a1)
+        	a.del.cum = Cumulate.vec( a.del * (1:p^2) )
+        	b = Cumulate.vec(a1)
+        	if(b[p^2] <= 1){
+        		x = a1    
+        	} else {
+        		id = (a.del.cum < 1)
+        		K = sum(id) + 1
+        		y = (b[K] - 1)/K
+        		x = rep(0, p^2)
+        		x[1:K] = a1[1:K] - y
+        	}
+        	B1 = sign(a) * solve(T.mat) %*% x
+        	A.result = matrix(B1, p, p)
+        	A.result = (A.result + t(A.result))/2
+        	return(A.result)
+        }
+        
+        T.matrix = function(A){
+        	p = dim(A)[1]
+        	a = as.vector(A)  
+        	id = order(a, decreasing = TRUE)
+        	T.mat = matrix(0, p^2, p^2)
+        	mat.id = cbind(1:p^2, id)
+        	T.mat[mat.id] = 1  
+        	return(T.mat)
+        }
+        
+        Delta.vec = function(a){
+        	# calculate the different between consective entries
+        	p = length(a)
+        	a.result = rep(0, p)
+        	a.result[p] = a[p]
+        	a.result[1:(p - 1)] = a[1:(p - 1)] - a[2:p]
+        	return(a.result)
+        }
+        
+        Cumulate.vec = function(a){
+        	# calculate the cumulate sum for a vector
+        	p = length(a)
+        	b = rep(0, p)
+        	for(i in 1:p){
+        		b[i] = sum(a[1:i])
+        	}
+        	return(b)
+        }
+        ''')
+
+r_MatrixMaxProj = robjects.globalenv['MatrixMaxProj']
+
+
+# import rpy2's package module
+import rpy2.robjects.packages as rpackages
+
+# import R's utility package
+utils = rpackages.importr('utils')
+
+# select a mirror for R packages
+utils.chooseCRANmirror(ind=145) # select the first mirror in the list
+utils.install_packages("QUIC")
+
+robjects.r('''
+           library("QUIC")
+           ''')
+
+r_QUIC = robjects.r['QUIC']
+
+
--- simulation_one_subject.py
+++ simulation_one_subject.py
@@ -12,15 +12,19 @@ import os
 sys.path.append(os.path.abspath('..'))
 from two_d_graphs.myfunctions import *
 from two_d_graphs.posdef import *
+from two_d_graphs.MatrixMaxProj import *
 from scipy import io
 
-p = 100
+from statsmodels.stats.moment_helpers import cov2corr
+
+p = 50
 d = 3
 n_change = 3
-len_t = 101
+len_t = 201
 n = 1
-h = 5.848/np.cbrt(len_t)
+h = 5.848/np.cbrt(len_t-1)
 sigma = 0.5
+phi = 0.5
 
 gG = getGraph(p, d)
 S, A = gG
@@ -38,7 +42,8 @@ Y = X+E0
 Y_list = [Y]
 for time_ix in range(1,len_t):
     C = C_T_list[time_ix]*sigma**2
-    E = E0 + np.random.multivariate_normal(mean = np.zeros(p), cov = C, size = n)
+#    E = np.random.multivariate_normal(mean = np.zeros(p), cov = C, size = n)
+    E = phi*E0 + np.random.multivariate_normal(mean = np.zeros(p), cov = C, size = n)
     E0 = E
     Y = X+E
     Y_list.append(Y)
@@ -47,14 +52,20 @@ Y_array = np.array(Y_list) # t by n by p
 Y_mean = np.mean(Y_array, 0)
 E_array = Y_array - Y_mean # t by n by p
 
-Y_array = Y_array.transpose([1,0,2])
+Y_array = Y_array.transpose([1,0,2]) # n by t by p
 E_array = E_array.transpose([1,0,2])
 
+#####S_X0
 YYT = np.array([[np.matmul(Y_array[i][t][:,None], Y_array[i][t][None,:]) for t in range(len_t)] for i in range(n)])
 EET = np.array([[np.matmul(E_array[i][t][:,None], E_array[i][t][None,:]) for t in range(len_t)] for i in range(n)])
 
 YYT0 = YYT[0]
 EET0 = EET[0]
+
+# S_X0 = nearestPD(np.mean(YYT0 - EET0, 0)) # psd check np.linalg.cholesky
+S_X0 = np.array(r_MatrixMaxProj(robjects.r.matrix(robjects.FloatVector(np.mean(YYT0 - EET0, 0).ravel()), nrow=p)))
+
+#####S_X_kernel
 S_Y0_list = []
 S_E0_list = []
 for m in range(len_t):
@@ -65,7 +76,7 @@ for m in range(len_t):
         S_Yl = YYT0[l]
         S_El = EET0[l]
 #            S_ml = S_Y[m*p+np.array(range(p))[:, None], l*p+np.array(range(p))[None, :]]
-        w_ml = np.exp(-np.square(m - l)/h)
+        w_ml = np.exp(-np.square((m - l)/(h*(len_t-1))))
         print(w_ml)
         S_Y0 = S_Y0 + S_Yl*w_ml
         S_E0 = S_E0 + S_El*w_ml
@@ -75,16 +86,54 @@ for m in range(len_t):
     S_E0 = S_E0/w
     S_Y0_list.append(S_Y0)
     S_E0_list.append(S_E0)
+S_Y0_array = np.array(S_Y0_list)
+S_E0_array = np.array(S_E0_list)
+
+# S_X_kernel = nearestPD(np.mean(S_Y0_array - S_E0_array, 0)) # psd check np.linalg.cholesky
+S_X_kernel = np.array(r_MatrixMaxProj(robjects.r.matrix(robjects.FloatVector(np.mean(S_Y0_array - S_E0_array, 0).ravel()), nrow=p)))
+alpha_1 = alpha_max(S_X_kernel)
+r_S_X_kernel = robjects.r.matrix(robjects.FloatVector(S_X_kernel.ravel()), nrow=p)
+r_path_vec = robjects.FloatVector(np.logspace(np.log10(1), np.log10(0.01), 50))
+r_rho_mtx = robjects.r.matrix(robjects.FloatVector(((np.triu(np.ones((p,p)), 1) + np.tril(np.ones((p,p)), -1))*alpha_1).ravel()), nrow = p)
+result_S_X_kernel = r_QUIC(r_S_X_kernel, r_rho_mtx, r_path_vec)
+Omega_kernel = np.array(result_S_X_kernel[0])
+
+getF1(A0, Omega_kernel[:,:,10])
+
+#####S_X_cov
+Y_array = Y_array.reshape([n, len_t*p])
+vec_Y_outer = [np.outer(Y_array[i], Y_array[i]) for i in range(n)]
+
+S_Y = vec_Y_outer[0]
+S_X_cov = np.zeros([p, p]) # Sigma_0
+w = 0
+for m in range(len_t):
+    for l in range(len_t): 
+#    for l in [i for i in range(len_t) if i != m]:
+        S_ml = S_Y[m*p+np.array(range(p))[:, None], l*p+np.array(range(p))[None, :]]
+        w_ml = 1-np.exp(-np.square((m - l)/(h*(len_t-1))))
+        print m,l,w_ml
+        w = w + w_ml
+        S_X_cov = S_X_cov + S_ml*w_ml
+S_X_cov = S_X_cov/w
+S_X_cov = np.array(r_MatrixMaxProj(robjects.r.matrix(robjects.FloatVector(S_X_cov.ravel()), nrow=p)))
+
+# shrunk covariance
+S_X_cov_shrunk = cov.shrunk_covariance(S_X_cov, 0.7)
+alpha_1 = alpha_max(S_X_cov_shrunk)
+alpha_0 = alpha_1*0.01
+alphas = np.logspace(np.log10(alpha_1), np.log10(alpha_0), 50)
+Omega_cov_shrunk = [cov.graph_lasso(S_X_cov_shrunk, alpha)[1] for alpha in alphas]
+getF1(A0, Omega_cov_shrunk[49])
 
 
+# quic
+alpha_1 = alpha_max(S_X_cov)
+r_S_X_cov = robjects.r.matrix(robjects.FloatVector(S_X_cov.ravel()), nrow=p)
+r_path_vec = robjects.FloatVector(np.logspace(np.log10(1), np.log10(0.01), 50))
+r_rho_mtx = robjects.r.matrix(robjects.FloatVector(((np.triu(np.ones((p,p)), 1) + np.tril(np.ones((p,p)), -1))*alpha_1).ravel()), nrow = p)
+result_S_X_cov = r_QUIC(r_S_X_cov, r_rho_mtx, r_path_vec)
+Omega_cov = np.array(result_S_X_cov[0])
 
-
-
-
-
-
-
-
-
-
+getF1(A0, Omega_cov[:,:,10])
 
